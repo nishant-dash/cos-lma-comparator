a258457108bf21c7c354ad693182822337e180d3
Danny Cocks <danny.cocks@canonical.com>
2023-11-08 13:47:23 +0200

    Temp working

--- cos-lma-comparator/comparator.py
+++ cos-lma-comparator/comparator.py
@@ -1,3 +1,26 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class NRPEData:
+    juju_model: str
+    juju_unit: str
+    alert_state: int # Maybe str?
+    alert_check_name: str
+    alert_time: float # Unix time
+
+
+
+
+
+
+
+
+
+
+
+
+
 from rules import PrometheusClient
 from services import NagiosClient
 
