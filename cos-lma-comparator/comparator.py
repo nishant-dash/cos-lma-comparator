9098bc05fb48edfeeeceb1bff20b7cc767d1df8b
Danny Cocks <danny.cocks@canonical.com>
2023-11-09 10:58:30 +0200

    Comment out comparator

--- cos-lma-comparator/comparator.py
+++ cos-lma-comparator/comparator.py
@@ -18,29 +18,26 @@ class NRPEData:
 
 
 
+# from rules import PrometheusClient
+# from services import NagiosClient
 
+# nagios_host_context = "bootstack-a-b"
 
+# prom_data = PrometheusClient()
+# nagios_data = NagiosClient(nagios_host_context)
 
-from rules import PrometheusClient
-from services import NagiosClient
+# rules = prom_data.rules
+# services = nagios_data.services
 
-nagios_host_context = "bootstack-a-b"
+# cos_units = {}
+# for r in rules:
+#     cos_units.add(r.juju_unit)
 
-prom_data = PrometheusClient()
-nagios_data = NagiosClient(nagios_host_context)
+# nagios_units = set()
+# for s in services:
+#     nagios_units.add(s.juju_unit)
 
-rules = prom_data.rules
-services = nagios_data.services
-
-cos_units = {}
-for r in rules:
-    cos_units.add(r.juju_unit)
-
-nagios_units = set()
-for s in services:
-    nagios_units.add(s.juju_unit)
-
-for u in nagios_units:
-    if not u in cos_units:
-        print(f"{u} is missing in COS alerts")
+# for u in nagios_units:
+#     if not u in cos_units:
+#         print(f"{u} is missing in COS alerts")
 
