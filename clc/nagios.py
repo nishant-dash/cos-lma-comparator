96ef78fb5eb2e77877c53ee0607adf6f145c30a8
Danny Cocks <danny.cocks@canonical.com>
2023-11-10 11:18:42 +0200

    Fix init of NRPEData with self and ignore nagios_context None

--- clc/nagios.py
+++ clc/nagios.py
@@ -7,7 +7,7 @@ from .utils.structures import NRPEData
 
 class NagiosService(NRPEData):
     def __init__(self, nagios_service_json, nagios_context=None):
-        super().__init__(self)
+        super().__init__()
         self.set_json(nagios_service_json)
 
         self.nagios_context = nagios_context
@@ -19,6 +19,8 @@ class NagiosService(NRPEData):
         return json.dumps(self.__dict__, indent=2)
 
     def __context_match(self):
+        if self.nagios_context is None:
+            return False
         return self._host_display_name.startswith(self.nagios_context)
 
     def __extract_unit_name(self):
@@ -33,7 +35,7 @@ class NagiosService(NRPEData):
 
 
 class NagiosServices:
-    def __init__(self, nagios_services_json, nagios_context=None):
+    def __init__(self, nagios_services_json, args, nagios_context=None):
         self._alerts = []
 
         for service in nagios_services_json:
